4342db726a9f727bea622b9d4b74e91430026819
althurayasaud <althurayaalrawahi3@gmail.com>
2026-05-29 14:31:46 +0400

    //Error loading patients

--- app/src/main/java/com/example/myflex/TherapistDashboardActivity.java
+++ app/src/main/java/com/example/myflex/TherapistDashboardActivity.java
@@ -142,7 +142,7 @@ public class TherapistDashboardActivity extends AppCompatActivity {
                     }
                 })
                 .addOnFailureListener(e -> {
-                    Toast.makeText(this, "خطأ في تحميل المرضى: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Error loading patients: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                 });
     }
 
